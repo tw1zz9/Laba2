e34834195ffe2de9bbc6ce793c143056bd4078da
host1l3 <timvitkulevskij@gmail.com>
2025-10-22 21:35:18 +0700

    made 1-st part
    Первая часть работы

---
+++ Laba2/BetterString.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    public class BetterString
+    {
+        public string Data { get; set; }
+
+        public BetterString() => Data = "";
+        public BetterString(string data) => Data = data;
+
+        // Операторы преоразования типов (явный и неявный)
+        public static implicit operator BetterString(string data)
+            => new BetterString(data);
+
+        public static explicit operator string(BetterString bs)
+            => bs.Data;
+
+        // Парные операторы сравнения строк по длине
+        public static bool operator <(BetterString a, BetterString b) => a.Data.Length < b.Data.Length;
+        public static bool operator >(BetterString a, BetterString b) => a.Data.Length > b.Data.Length;
+
+        // Оператор +, добавляющий число к строке с расстоянием между ними в один пробел
+        public static BetterString operator +(BetterString a, int b)
+        {
+            a.Data += $" {b}";
+            return a;
+        }
+
+        // Оператор декремента, удаляющий последний символ в строке
+        public static BetterString operator --(BetterString a)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (var i = 0; i < a.Data.Length - 1; i++) stringBuilder.Append(a.Data[i]);
+            a.Data = stringBuilder.ToString();
+            return a;
+        }
+
+        // Опертатор умножения, который переписывает строку так, чтобы в ней были символы такие же, как и newCh
+        public static BetterString operator *(BetterString a, char newCh)
+        {
+            StringBuilder newStr = new StringBuilder();
+            for (var i = 0; i < a.Data.Length; i++)
+                newStr.Append(newCh);
+            a.Data = newStr.ToString();
+            return a;
+        }
+
+        public override string ToString() => Data;
+    }
+
+    public static class ExtendedString
+    {
+        // Метод расширения для подтверждения о наличии символов в строке
+        public static bool IsThere(this string str, params char[] ch)
+        {
+            foreach (var expSymbol in ch)
+            {
+                foreach (var val in str)
+                    if (val == expSymbol) return true;
+            }
+            return false;
+        }
+        
+        // Метод расширения для переписи строки без знаков препинания
+        public static string DeletePunctuationMarks(this string str)
+        {
+            return string.IsNullOrEmpty(str) ? 
+                str : 
+                new string(str.Where(c => !char.IsPunctuation(c)).ToArray());
+        }
+    }
+}
--- Laba2/Program.cs
+++ Laba2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BetterString bs1 = new BetterString("CHARIZARD");
+            BetterString bs2 = new BetterString("CHAMELEON");
+
+            Console.WriteLine(bs1 < bs2);
+            Console.WriteLine(bs2 < bs1);
+
+            bs1 = bs1 + 3;
+            Console.WriteLine(bs1);
+
+            bs2 = bs2 + 1337;
+            Console.WriteLine(bs2);
+
+            for (var i = 0; i < 5; i++) bs2--;
+
+            bs2 += 4;
+            Console.WriteLine(bs2);
+
+            bs1 *= '\\';
+            Console.WriteLine(bs1);
+
+            Console.WriteLine(bs1.Data.IsThere('S'));
+            Console.WriteLine(bs1.Data.IsThere('\\'));
+
+            Console.WriteLine(bs2.Data.IsThere('A'));
+
+            bs2.Data += ",./'' фцфцв.,./,/.,][];'.!@#!%";
+            bs2.Data = bs2.Data.DeletePunctuationMarks();
+
+            Console.WriteLine(bs2);
         }
     }
 }
